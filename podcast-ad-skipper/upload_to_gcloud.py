768ae187df69671796fcb070f399d0fc237580ee
Nuria Aires <89844643+nairesc@users.noreply.github.com>
2024-10-12 19:29:44 +0100

    update function name + fix auth_gc function

--- podcast-ad-skipper/upload_to_gcloud.py
+++ podcast-ad-skipper/upload_to_gcloud.py
@@ -1,22 +1,42 @@
 import json
 import os
+import sys
+from termcolor import colored
 
+from google.auth.exceptions import GoogleAuthError
 from google.cloud import storage
 from google.oauth2 import service_account
 
 
 def auth_gc():
-    # wip
-    with open('gcp/podcast-ad-skipper-0dd8dd2c5ac1.json') as source:
-        info = json.load(source)
+    """Authenticates and returns a Google Cloud Storage client using service account credentials"""
+    try:
+        with open('gcp/podcast-ad-skipper-0dd8dd2c5ac1.json') as source:
+            info = json.load(source)
 
-    storage_credentials = service_account.Credentials.from_service_account_info(info)
+        storage_credentials = service_account.Credentials.from_service_account_info(info)
 
-    storage_client = storage.Client(project=os.environ.get('GCP_PROJECT_ID'), credentials=storage_credentials)
+        storage_client = storage.Client(project=os.environ.get('GCP_PROJECT_ID'), credentials=storage_credentials)
 
-    return storage_client
+        print("Authenticated successfully! ✅")
+        return storage_client
 
-def upload_files_to_gcloud(bucket_name, filenames, blobname):
+    except FileNotFoundError:
+        print("Error: The specified credentials file 'gcp/file_name.json' was not found.")
+        print(colored("Failed to authenticate with Google Cloud Storage ❌", "red"))
+        sys.exit(1)
+
+    except GoogleAuthError as e:
+        print(f"Error: Authentication failed with Google Cloud: {e}")
+        print(colored("Failed to authenticate with Google Cloud Storage ❌", "red"))
+        sys.exit(1)
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        print(colored("Failed to authenticate with Google Cloud Storage ❌", "red"))
+        sys.exit(1)
+
+def upload_clips_gcs(client, bucket_name, filenames, blobname):
     """Upload every file in a list to a bucket, concurrently in a process pool.
 
     Each blob name is derived from the filename, not including the
@@ -42,17 +62,10 @@ def upload_files_to_gcloud(bucket_name, filenames, blobname):
     # some CPU and memory resources until finished. Threads can be used instead
     # of processes by passing `worker_type=transfer_manager.THREAD`.
     # workers=8
-    with open('gcp/podcast-ad-skipper-0dd8dd2c5ac1.json') as source:
-        info = json.load(source)
-
-    storage_credentials = service_account.Credentials.from_service_account_info(info)
-
-    storage_client = storage.Client(project=os.environ.get('GCP_PROJECT_ID'), credentials=storage_credentials)
-
-    bucket = storage_client.bucket(bucket_name)
-
-    d = bucket.blob(blobname)
-    d.upload_from_file(filenames, content_type="audio/wav")
+    if client is not None:
+        bucket = client.bucket(bucket_name)
+        d = bucket.blob(blobname)
+        d.upload_from_file(filenames, content_type="audio/wav")
 
     # for name, result in zip(filenames, results):
     # The results list is either `None` or an exception for each filename in
