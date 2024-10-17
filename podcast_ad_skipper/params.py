90ed2871bbc8d865fd53bef44ffceb2091b48ede
Nuria Aires <89844643+nairesc@users.noreply.github.com>
2024-10-17 23:59:02 +0100

    params should be inside the module

---
+++ podcast_ad_skipper/params.py
@@ -0,0 +1,31 @@
+import os
+
+BUCKET_NAME  = os.environ.get("BUCKET_NAME")
+GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+GCP_REGION = os.environ.get("GCP_REGION")
+
+PREFIXES_PODCAST_AD_SKIPPER = [
+    'billionairepersonalitydisorder',
+    'bitcoinminingdecentralizationwiththedatumprotocolatoceanmining',
+    'ceo181',
+    'changesinthebigapple',
+    'differentdays',
+    'donaldtrumpjoinstheshow',
+    'dreamingofpolarnightinsvalbard',
+    'drewbarrymoreasksaboutboogers',
+    'electionspecial',
+    'farking&thelyingjester',
+    'guenthersteinerlifeontheothersideoff1',
+    'israelatwaroneyearon',
+    'knowingwhoyouare',
+    'nstaaf1',
+    'offmenu263',
+    'parentingHell908',
+    'quintabrunson',
+    'rabbitceojesselyuisnotthinkingtoofaradead',
+    'rupikauropensupifetinvisible',
+    'survivingahurricaneEp01',
+    'theproblemwithfancygrocerystoresftgwyneddstuartEp01',
+    'whatishiddeninyourwordsEp01',
+    'whenbitterbcamessweet'
+]
