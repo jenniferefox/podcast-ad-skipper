4b3a78f24e82c353f96329b0e2c66d13cc2888d9
Irene Gracia <irenucagp@hotmail.com>
2024-10-27 21:53:52 +0000

    updates to api file

--- podcast_ad_skipper/fast_api.py
+++ podcast_ad_skipper/fast_api.py
@@ -1,30 +1,31 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from podcast_ad_skipper.model import download_model_from_gcs
+import numpy as np
+import tensorflow as tf
+from keras import Model, Sequential, layers, regularizers, optimizers, applications, models
+from podcast_ad_skipper.params import *
+from podcast_ad_skipper.google_cloud import download_model_from_gcs
 
 #Basic structure of API - not yet functioning
 
 app = FastAPI()
 
-# app.state.model = load_model()
-# @app.get("/predict")
-# def predict(
-#         podcast_link: str
-#     ):
-#     """
-#     Takes podcast file, runs model and outputs podcast without ads.
-#     """
+gcs_uri = f"gs://{BUCKET_NAME_MODEL}/trained_model"
 
-#     X_pred = #define this
-#     X_preproc = #preprocess X_pred
 
-#     model = app.state.model
-#     assert model is not None
-#     y_pred = float(model.predict(X_preproc))
+app.state.model = download_model_from_gcs(gcs_uri)
+model = app.state.model
 
-#     return {
-#     'no_ad_podcast': y_pred
-#     }
+@app.get("/predict")
+def predict(spectrogram: np.ndarray, model):
+# FROM CODE IN FRONT END
+    model = app.state.model
+    X_pred_preprocessed = None
+    prediction = model.predict(X_pred_preprocessed)
+    return prediction
+
 
 @app.get("/")
 def root():
